947450c059f32c54a2a9fc00cb121235ef043b3a
嵇姝涵 <jishuhan@jishuhandeMacBook-Pro.local>
2023-03-17 16:22:14 +0800

    rabbit1

--- practical5/breed_like_rabbits.py
+++ practical5/breed_like_rabbits.py
@@ -1,12 +1,14 @@
 #Number of rabbits in the first generation with known conditions
+#a is the number of the generation,b is the number of rabbit
+#The while loop indicates the number of rabbits after each generation is increa$
+#With each new generation, the rabbit population doubles
+#Exit the loop when the number of rabbits is more than 100
+#Output the generations of rabbits at this time
 a=1
 b=2
-#The while loop indicates the number of rabbits after each generation is increased
 while 1==1:
  a+=1
- b=b*2
-#Exit the loop when the number of rabbits is more than 100 
+ b=b*2 
  if b>100:
-#Output the generations of rabbits at this time  
   print("The generation that over 100 rabbits have been born is",a)
   break
