2d5fe41c0d3d27b523690e3c70d3e0a58ae00a2c
嵇姝涵 <jishuhan@jishuhandeMacBook-Pro.local>
2023-03-17 16:27:19 +0800

    hexagon sequence

--- practical5/hexagons.py
+++ practical5/hexagons.py
@@ -1,6 +1,6 @@
 #The first five values of the hexagon sequence were calculated
-for n in range(1,6):
-#formula
- h=2*n*(2*n-1)/2
+#input formula
 #output result
+for n in range(1,6):
+ h=2*n*(2*n-1)/2
  print(h)
