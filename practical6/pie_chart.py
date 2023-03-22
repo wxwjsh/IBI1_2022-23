a4bb06e1b889726acf0190a0048415d02233845f
嵇姝涵 <jishuhan@jishuhandeMacBook-Pro.local>
2023-03-22 14:58:59 +0800

    colorful

--- practical6/pie_chart.py
+++ practical6/pie_chart.py
@@ -6,7 +6,8 @@
 import matplotlib.pyplot as plt
 labels='Comedy','Action','Romance','Fantasy','Science-fiction','Horror','Crime','Documentary','History','War'
 sizes=[73,42,38,28,22,19,18,12,8,7]
-explode=(0.03,0,0,0,0,0,0,0,0,0)
-plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=False,startangle=0)
+explode=(0.05,0,0,0,0,0,0,0,0,0)
+colors=['honeydew','mistyrose','lightyellow','lavender']
+plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=True,startangle=0,colors=colors)
 plt.axis('equal')
 plt.show()
