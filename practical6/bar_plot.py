13ffdfda4d4aac66593561e9e9fe117f2b9719b8
嵇姝涵 <jishuhan@jishuhandeMacBook-Pro.local>
2023-03-22 15:23:21 +0800

    bar_plot

--- practical6/bar_plot.py
+++ practical6/bar_plot.py
@@ -6,13 +6,12 @@
 import numpy as np
 import matplotlib. pyplot as plt
 N=8
-scores=(1,8,15,7,5,14,43,40)
+costs=(1,8,15,7,5,14,43,40)
 ind=np.arange(N)
 width=0.55
-p1= plt. bar(ind, scores, 
-width,color=['r','y','c','g','b','m','k','crimson'])
+p1= plt.bar(ind,costs,width,color=['r','y','c','g','b','m','k','crimson'])
 plt. ylabel('Cost(in $ billions)')
 plt.title('The cost of the Olympic Game')
-plt.xticks(ind,('Los Angeles 1984','Seoul 1998','Barcelona 1992','Atlanta1996','Sydnet 2000','Athens 2003','Beijing 2008','London 2012')) 
+plt.xticks(ind,('Los Angeles 1984','Seoul 1998','Barcelona 1992','Atlanta1996','Sydney 2000','Athens 2003','Beijing 2008','London 2012')) 
 plt.yticks (np.arange(0, 41, 5))
 plt. show()
