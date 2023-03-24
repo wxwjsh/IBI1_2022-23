c60ee43ba0e65b981cedef65ebb00e462c761007
嵇姝涵 <jishuhan@jishuhandeMacBook-Pro.local>
2023-03-24 17:01:09 +0800

    1

--- practical6/bar_plot.py
+++ practical6/bar_plot.py
@@ -3,15 +3,26 @@
 #Enter the width of the bar chart
 #Import the statistical chart title, x axis and y axis
 #Set the Y-axis to 0-40
+cities=['Los Angeles 1984','Seoul 1988','Barcelona 1992','Atlanta 1996','Sydney 2000','Athens 2003','Beijing 2008','London 2012']
+costs=(1,8,15,7,5,14,43,40)
+#arrange in order
+sorted_values=sorted(costs)
+print (sorted_values)
+#find from the internet
+#Redefining cities and spending in ascending order
+import numpy
+cities=numpy.array(cities)
+costs=numpy.array(costs)
+sortedcosts=costs.argsort()
+sorted_cities=cities[sortedcosts]
+sorted_costs=costs[sortedcosts]
 import numpy as np
 import matplotlib. pyplot as plt
-N=8
-costs=(1,8,15,7,5,14,43,40)
-ind=np.arange(N)
 width=0.55
-p1= plt.bar(ind,costs,width,color=['r','y','c','g','b','m','k','crimson'])
+p1= plt.bar(sorted_cities,sorted_costs,width,color=['r','y','c','g','b','m','k','crimson'])
 plt. ylabel('Cost(in $ billions)')
 plt.title('The cost of the Olympic Game')
-plt.xticks(ind,('Los Angeles 1984','Seoul 1998','Barcelona 1992','Atlanta1996','Sydney 2000','Athens 2003','Beijing 2008','London 2012')) 
+#plt.xticks(cities)
+plt.xlabel('cities')
 plt.yticks (np.arange(0, 41, 5))
 plt. show()
