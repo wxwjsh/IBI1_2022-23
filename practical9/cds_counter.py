70a3f2e6ac50be7b8052c5653e021bb4bfc043f7
嵇姝涵 <jishuhan@jishuhandeMacBook-Pro.local>
2023-04-11 17:02:19 +0800

    1

--- practical9/cds_counter.py
+++ practical9/cds_counter.py
@@ -1,6 +1,5 @@
 import re
-seq = ‘ATGCAATCGACTACGATCTGAGAGGGCCTAA’
+seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
 a=seq.find('ATG')
-count=seq.count(r'(TAG|TGA|TAA)',a)
 print(len(re.findall(r'(TAG|TGA|TAA)',seq[a:])))
 
