2a74e5433152d0d0db4b69f8651e9d5bd1d7dca8
嵇姝涵 <jishuhan@jishuhandeMacBook-Pro.local>
2023-04-12 21:49:34 +0800

    1

--- practical9/cds_counter.py
+++ practical9/cds_counter.py
@@ -1,5 +1,7 @@
 import re
 seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
+#First find the start codon ATG
 start=seq.find('ATG')
+#Find the termination codon ATG again
 print(len(re.findall(r'(TAG|TGA|TAA)',seq[start:])))
 
