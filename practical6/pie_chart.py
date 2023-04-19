b4277298061c0d5a97f6b1046515fa5aa245ddd1
嵇姝涵 <jishuhan@jishuhandeMacBook-Pro.local>
2023-04-19 17:37:03 +0800

    movie

--- practical6/pie_chart.py
+++ practical6/pie_chart.py
@@ -1,3 +1,11 @@
+#make a dictionary with 10 keys and 10 values
+favor_movie={'Comedy' :73,'Action' :42,' Romance' :38,'Fantasy' :28,'Science-fiction':22,'Horro':19,'Crime':18,'Document':12,'History':8,'War':7}
+print(favor_movie)
+#When the corresponding key is entered, the value is matched
+movie=input("input a movie:")
+students=favor_movie.get(movie)
+print(students)
+
 #To make a pie chart, type key1-key10 as label
 #Then enter value1-value10 as the size of the pie chart
 #Enter the distance from the center of the pie block
@@ -11,3 +19,8 @@ colors=['honeydew','mistyrose','lightyellow','lavender']
 plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=True,startangle=0,colors=colors)
 plt.axis('equal')
 plt.show()
+
+#Create a new dictionary that arranges the contents of the original directory in descending order of value， printing the first movie type and the number of favorite students
+#Code source network
+best= sorted(favor_movie.items(),key=lambda x: x[1], reverse=True)
+print('Their favorite movie and the number of students is : ',best[0])
