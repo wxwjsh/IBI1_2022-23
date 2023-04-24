8af75e51faa6aa0cfc3c315e757ec11967fd0d8e
嵇姝涵 <jishuhan@jishuhandeMacBook-Pro.local>
2023-04-24 15:38:36 +0800

    count_cds_genes

--- practical9/count_cds_genes.py
+++ practical9/count_cds_genes.py
@@ -3,11 +3,11 @@ import re
 stop_codon=input("Please input a stop codon(TAA/TAG/TGA):")
 #Open the original file and define the variable name file
 import os
-os.chdir("/Users/jishuhan/.git/IBI1_2022-23/IBI1_2022-23/IBI1_2022-23/practical9")
+os.chdir("/Users/jishuhan/.git/IBI1_2022-23/IBI1_2022-23/practical9")
 dna=open("Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa",'r')
 data = open("{}_stop_genes.fa".format (stop_codon), "w")
 #Use the compile command to search for the first space up to >, defined as 
-dna _name
+
 dna_name=re.compile(r'>(\S+)')
 #Defining variables and initial count values
 finall_dna_name=''
