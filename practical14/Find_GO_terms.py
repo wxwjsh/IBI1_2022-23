df8ab0871349a1738d4942e3222078fe74e8d229
wxwjsh <127287908+wxwjsh@users.noreply.github.com>
2023-05-16 19:05:33 +0800

    Add files via upload

---
+++ practical14/Find_GO_terms.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 17:54:44 2023
+
+@author: jishuhan
+"""
+
+import xml.dom.minidom
+from openpyxl import Workbook
+
+# Find and load xml documents go_obo.xml'
+xml_file_path =r'/Users/jishuhan/Desktop/go_obo.xml'
+dom = xml.dom.minidom.parse(xml_file_path)
+
+# find <term> elements
+term_elements = dom.getElementsByTagName('term')
+
+# Create an Excel that can be filled
+workbook = Workbook()
+sheet = workbook.active
+
+# create a sheet with four column named 'GO ID', 'Term Name', 'Definition' and 'Number of Child Nodes'.
+sheet['A1'] = 'GO ID'
+sheet['B1'] = 'Term Name'
+sheet['C1'] = 'Definition'
+sheet['D1'] = 'Number of Child Nodes'
+
+# Fill in the data from the second row
+row_index = 2
+
+#Inside the count_child_nodes() function, it iterates over all the <term> elements and checks if the <is_a> 
+#element within the term has a value that matches the go_id of the input term. If there is a match, it increments 
+#the count variable and recursively calls the count_child_nodes() function for the child term identified by the <id> element.
+#This process continues until all child nodes have been counted.
+def count_child_nodes(term_id):
+    count = 0
+    for term_element in term_elements:
+        is_a_elements = term_element.getElementsByTagName('is_a')
+        for is_a_element in is_a_elements:
+            if is_a_element.firstChild.data == term_id:
+                count += 1
+                count += count_child_nodes(term_element.getElementsByTagName('id')[0].firstChild.data)
+    return count
+
+
+# for all <term> elements, find <defstr> elements
+for term_element in term_elements:
+    defstr_element = term_element.getElementsByTagName('defstr')[0]
+    #find autophagosome in <defstr>
+    #firstChild means: gets the first child of the ID element, which may be a text node containing the term ID of the child node.
+    #find the <id>, <name>, and <defstr> elements
+    if 'autophagosome' in defstr_element.firstChild.data:
+        id_element = term_element.getElementsByTagName('id')[0]
+        name_element = term_element.getElementsByTagName('name')[0]
+        go_id = id_element.firstChild.data
+        term_name = name_element.firstChild.data
+        definition = defstr_element.firstChild.data
+        
+        # Count the number of child nodes
+        num_of_child_nodes = count_child_nodes(go_id)
+               
+        #Writing data to excal tables
+        sheet.cell(row=row_index, column=1).value = go_id
+        sheet.cell(row=row_index, column=2).value = term_name
+        sheet.cell(row=row_index, column=3).value = definition
+        sheet.cell(row=row_index, column=4).value = num_of_child_nodes
+        row_index += 1
+
+#save the sheet
+excel_file_path = r'/Users/jishuhan/Desktop\IBIcode.xlsx'
+workbook.save(excel_file_path)
